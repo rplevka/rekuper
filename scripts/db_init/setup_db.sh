0e3d26bf23b4bc47859c24cfb4d10077601308bd
Roman Plevka <rplevka@rplevka-thinkpadt14gen4.remote.csb>
2024-11-13 12:01:02 +0100

    create podman network as a part of setup_db.sh helper script

--- scripts/db_init/setup_db.sh
+++ scripts/db_init/setup_db.sh
@@ -1,1 +1,2 @@
-podman run --rm -e POSTGRES_PASSWORD=changeme -v ./init-user-db.sh:/docker-entrypoint-initdb.d/init-user-db.sh:z -p 25432:5432 postgres:17
+podman network create rekuper
+podman run --network rekuper --rm -e POSTGRES_PASSWORD=changeme -v ./init-user-db.sh:/docker-entrypoint-initdb.d/init-user-db.sh:z -p 25432:5432 postgres:17
