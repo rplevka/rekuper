29956393a74c1fd2a1d12bb8a3fe334d55b08a42
Roman Plevka <rplevka@redhat.com>
2024-11-15 18:43:41 +0100

    update db init script to set proper permissions for RO user

--- scripts/db_init/setup_db.sh
+++ scripts/db_init/setup_db.sh
@@ -1,2 +1,12 @@
 podman network create rekuper
-podman run --name rekuper_db --network rekuper --rm -e POSTGRES_PASSWORD=changeme -v ./init-user-db.sh:/docker-entrypoint-initdb.d/init-user-db.sh:z -p 25432:5432 postgres:17
+podman run \
+    --rm \
+    --name rekuper_db \
+    --network rekuper \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=changeme \
+    -e POSTGRES_RO_USER=telemetry \
+    -e POSTGRES_RO_PASSWORD=fero \
+    -v ./init-user-db.sh:/docker-entrypoint-initdb.d/init-user-db.sh:z \
+    -p 25432:5432 \
+    postgres:17 postgres -c log_statement=all
